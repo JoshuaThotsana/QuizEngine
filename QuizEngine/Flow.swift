2f7c4936723b0d1c62f3c660f7c2f0d7e3688d84
Joshua Thotsana Mabotsa <joshua.mabotsa@umuzi.org>
2020-12-10 18:15:20 +0200

    Up To Lecture 4

---
+++ QuizEngine/Flow.swift
@@ -0,0 +1,42 @@
+//
+//  Flow.swift
+//  QuizEngine
+//
+//  Created by Thotsana Mabotsa on 2020/12/10.
+//
+
+import Foundation
+
+protocol Router {
+	typealias AnswerCallback = (String) -> Void
+	func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+}
+
+class Flow {
+	private let router: Router
+	private  let questions: [String]
+	
+	init(questions: [String], router: Router) {
+		self.questions = questions
+		self.router = router
+	}
+	
+	func start() {
+		if let firstQuestion = questions.first {
+			router.routeTo(question: firstQuestion, answerCallback: routeNext(from: firstQuestion))
+		}
+	}
+	
+	private func routeNext(from question: String) -> Router.AnswerCallback {
+		return { [weak self] _ in
+			guard let strongSelf = self else { return }
+			if let currentQuestionIndex = strongSelf.questions.firstIndex(of: question) {
+				if currentQuestionIndex+1 < strongSelf.questions.count {
+					let nextQuestion = strongSelf.questions[currentQuestionIndex+1]
+					strongSelf.router.routeTo(question: nextQuestion, answerCallback: strongSelf.routeNext(from: nextQuestion))
+				}
+			}
+		}
+	}
+}
+
